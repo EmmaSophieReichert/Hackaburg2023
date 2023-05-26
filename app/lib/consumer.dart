d54204866969c29e4e51cb19161ee4d60f582fda
Anton Achhammer <AchhammerAntonMichael@web.de>
2023-05-26 13:59:43 +0200

    added Consumerlist & Person <-- but with errors

--- app/lib/consumer.dart
+++ app/lib/consumer.dart
@@ -7,3 +7,16 @@ class Consumer {
   Consumer(this.name, this.power, this.unit, this.iconID);
 
 }
+
+class ConsumerList {
+  //TODO: iconIDs anpassen
+  Consumer waschmaschine = new Consumer{"Waschmaschine", 0.58, "kWh", 0};
+  Consumer backofen = new Consumer{"Backofen", 0.74, "kWh", 0};
+  Consumer trockner = new Consumer{"Trockner", 5.04, "kWh", 0};
+  Consumer herd = new Consumer{"Herd", 1.5, "kWh", 0};
+  Consumer geschirrspueler = new Consumer{"Geschirrspülmaschine", 1.05, "kWh", 0};
+}
+
+class Person {
+  final consumerList = <Consumer>[];
+}
